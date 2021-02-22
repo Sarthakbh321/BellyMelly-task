e1105f4d964ca6efb3e558cc3831debe47b7638c
Sarthak <sarthakbh321@gmail.com>
2021-02-22 22:10:29 +0530

    initialized material ui theme

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: "#2242A4",
+		},
+	},
+});
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
