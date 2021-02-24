8da81d63341bb5115c4c69c89d3076ed785e20e1
Sarthak <sarthakbh321@gmail.com>
2021-02-24 18:39:21 +0530

    fixed action bar

--- src/components/ActionBar/ActionBar.js
+++ src/components/ActionBar/ActionBar.js
@@ -1,4 +1,4 @@
-import { Container, Typography } from "@material-ui/core";
+import { Container, Grid, Typography } from "@material-ui/core";
 import { Done, SaveAlt, VisibilityOutlined } from "@material-ui/icons";
 import React from "react";
 import ActionButton from "../ActionButton/ActionButton";
@@ -7,27 +7,39 @@ import styles from "./ActionBar.module.css";
 const ActionBar = () => {
 	return (
 		<Container className={styles.action_bar}>
-			<Typography
-				variant="h5"
-				style={{ color: "#33393F", fontWeight: "bold" }}
-			>
-				Create New Listing
-			</Typography>
-			<div className={styles.action_btn_div}>
-				<ActionButton
-					text="Preview"
-					icon={<VisibilityOutlined style={{ fill: "#858D95" }} />}
-				/>
-				<ActionButton
-					text="Save"
-					icon={<SaveAlt style={{ fill: "#858D95" }} />}
-				/>
-				<ActionButton
-					text="Publish"
-					icon={<Done style={{ fill: "white" }} />}
-					primary
-				/>
-			</div>
+			<Grid container spacing={3}>
+				<Grid item xs={12} sm={12} md={7}>
+					<Typography
+						variant="h5"
+						style={{ color: "#33393F", fontWeight: "bold" }}
+					>
+						Create New Listing
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					sm={12}
+					md={5}
+					className={styles.action_btn_div}
+				>
+					<ActionButton
+						text="Preview"
+						icon={
+							<VisibilityOutlined style={{ fill: "#858D95" }} />
+						}
+					/>
+					<ActionButton
+						text="Save"
+						icon={<SaveAlt style={{ fill: "#858D95" }} />}
+					/>
+					<ActionButton
+						text="Publish"
+						icon={<Done style={{ fill: "white" }} />}
+						primary
+					/>
+				</Grid>
+			</Grid>
 		</Container>
 	);
 };
