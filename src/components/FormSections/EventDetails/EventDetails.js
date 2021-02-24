78ce638365ad9f99ac01a743650df5bf9ba222dd
Sarthak <sarthakbh321@gmail.com>
2021-02-24 22:38:34 +0530

    event details done

--- src/components/FormSections/EventDetails/EventDetails.js
+++ src/components/FormSections/EventDetails/EventDetails.js
@@ -1,5 +1,6 @@
 import { Grid, Typography } from "@material-ui/core";
 import React from "react";
+import InputField from "../../InputField/InputField";
 import styles from "./EventDetails.module.css";
 
 const EventDetails = () => {
@@ -14,7 +15,80 @@ const EventDetails = () => {
 						Basic Details for your listing descriptions
 					</Typography>
 				</Grid>
-				<Grid item sm={12} md={7}></Grid>
+				<Grid item sm={12} md={7}>
+					<div style={{ marginBottom: "40px" }}>
+						<Grid container spacing={3}>
+							<Grid item xs={12} sm={12} md={6}>
+								<Typography
+									variant="body1"
+									className={styles.field_header}
+								>
+									Price / entry{" "}
+									<span style={{ color: "red" }}>
+										{" * "}
+									</span>
+								</Typography>
+								<InputField placeholder="Price / entry" />
+							</Grid>
+							<Grid item xs={12} sm={12} md={6}>
+								<Typography
+									variant="body1"
+									className={styles.field_header}
+								>
+									Maximum Participants{" "}
+								</Typography>
+								<InputField placeholder="Maximum participants" />
+							</Grid>
+						</Grid>
+					</div>
+					<div style={{ marginBottom: "40px" }}>
+						<Grid container spacing={3}>
+							<Grid item xs={12} sm={12} md={6}>
+								<Typography
+									variant="body1"
+									className={styles.field_header}
+								>
+									Day of event{" "}
+								</Typography>
+								<InputField placeholder="(change)" />
+							</Grid>
+						</Grid>
+					</div>
+					<div style={{ marginBottom: "40px" }}>
+						<Grid container spacing={3}>
+							<Grid item xs={12} sm={12} md={6}>
+								<Typography
+									variant="body1"
+									className={styles.field_header}
+								>
+									Event Duration{" "}
+								</Typography>
+								<InputField placeholder="1 hr" />
+							</Grid>
+							<Grid item xs={12} sm={12} md={6}>
+								<Typography
+									variant="body1"
+									className={styles.field_header}
+								>
+									Event Timings
+								</Typography>
+								<div style={{ display: "flex" }}>
+									<InputField />
+									<span
+										style={{
+											margin: "0 5px",
+											display: "inline-flex",
+											alignItems: "center",
+										}}
+									>
+										to
+									</span>
+									<InputField />
+								</div>
+							</Grid>
+						</Grid>
+					</div>
+				</Grid>
 			</Grid>
 		</div>
 	);
